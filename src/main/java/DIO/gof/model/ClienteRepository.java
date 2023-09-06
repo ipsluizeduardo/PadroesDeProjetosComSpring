142ba7dc18a12f271c718bf6052faa4adf9f97dc
ipsluizeduardo <ipsluizeduardo@gmail.com>
2023-09-06 11:13:39 -0300

    Exemplo

---
+++ src/main/java/DIO/gof/model/ClienteRepository.java
@@ -0,0 +1,9 @@
+package DIO.gof.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+
+}
---
+++ src/main/java/DIO/gof/model/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package DIO.gof.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends CrudRepository<Endereco, String> {
+
+}
